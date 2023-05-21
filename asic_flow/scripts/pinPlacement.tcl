6835de5ca74c72b08f8138baa8456ed9ab6ab5d5
Abarajithan Gnaneswaran <ee260csp23ax@ieng6-ece-01.ucsd.edu>
2023-05-20 18:48:30 -0700

    Update floorplan and pin placement

--- asic_flow/scripts/pinPlacement.tcl
+++ asic_flow/scripts/pinPlacement.tcl
@@ -1,8 +1,9 @@
 getPinAssignMode -pinEditInBatch -quiet
 setPinAssignMode -pinEditInBatch true
 
-editPin -pinWidth 0.2 -pinDepth 0.6 -fixOverlap 1 -unit MICRON -spreadDirection        clockwise -side Left   -layer 3 -spreadType center -spacing 0.8 -pin {clk rstn copy k x*}
-editPin -pinWidth 0.2 -pinDepth 0.6 -fixOverlap 1 -unit MICRON -spreadDirection counterclockwise -side Right  -layer 3 -spreadType center -spacing 0.8 -pin {y*}
+editPin -pinWidth 0.2 -pinDepth 0.6 -fixOverlap 1 -unit MICRON -spreadDirection        clockwise -side Top   -layer 3 -spreadType center -spacing 0.8 -pin {clk rstn copy}
+editPin -pinWidth 0.2 -pinDepth 0.6 -fixOverlap 1 -unit MICRON -spreadDirection        clockwise -side Left   -layer 3 -spreadType center -spacing 0.8 -pin {x*}
+editPin -pinWidth 0.2 -pinDepth 0.6 -fixOverlap 1 -unit MICRON -spreadDirection counterclockwise -side Right  -layer 3 -spreadType center -spacing 0.8 -pin {k y*}
 
 setPinAssignMode -pinEditInBatch false
 getPinAssignMode -pinEditInBatch -quiet
